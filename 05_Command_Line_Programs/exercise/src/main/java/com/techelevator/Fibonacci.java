b971728e8af142007b09c3adb333140e447ec7e5
Ishwor <ishwor.tuladhar@gmail.com>
2022-02-25 14:20:39 -0500

    Submitted 05 Command Line Programs exercise

--- 05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ 05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -7,14 +7,15 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 
-		System.out.print("Please enter a number: ");
+		System.out.print("Please enter the fibonacci number: ");
 		String userInputData = in.nextLine();
 		int inputNum = Integer.parseInt(userInputData);
+
 		int previous = 0;
 		int current = 1;
 
 		if (inputNum <= 0)
-			System.out.println("Error! Fibonacci cannot perform");
+			System.out.println("0 1");
 			else{
 			System.out.print(previous);
 			while (inputNum > current){
