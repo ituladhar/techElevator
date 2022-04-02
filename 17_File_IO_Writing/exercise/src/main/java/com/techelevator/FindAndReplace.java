7c14cdf5350d94cb1438929e5af1402cdadb9d32
Ishwor <ishwor.tuladhar@gmail.com>
2022-04-02 16:38:03 -0400

    17_File_IO_Writing Exerises Submission.

--- 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,8 +1,71 @@
 package com.techelevator;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
     public static void main(String[] args) {
+        /*The program must prompt the user for the following values:
+        #1 The search word
+        #2 The word to replace the search word with
+        #3 The source file
+            This must be an existing file. If the user enters an invalid source file, the program indicates this to the user and exits.
+        #4 The destination file
+            The program creates a copy of the source file with the requested replacements at this location. If the file already exists, it must be overwritten. If the user enters an invalid destination file, the program indicates this to the user and exits.
+*/
+        try (Scanner userInput = new Scanner(System.in)) {
+            System.out.println("What is the search word?");
+            String searchWord = userInput.nextLine();
+            if (searchWord.isEmpty()) {
+                System.out.println("Not Valid Entry.");
+                System.exit(1);
+            }
+
+            System.out.println("What is the replacement word?");
+            String replacementWord = userInput.nextLine();
+            if (replacementWord.isEmpty()) {
+                System.out.println("Not Valid Entry.");
+                System.exit(1);
+            }
+
+            File sourceFilePath;
+            System.out.println("What is the source file?");
+            String path = userInput.nextLine();
+            sourceFilePath = new File(path);
+            if (!sourceFilePath.exists()) {
+                System.out.println(path + " does not exist.");
+                System.exit(1);
+            } else if (!sourceFilePath.isFile()) {
+                System.out.println(path + " is not a file.");
+                System.exit(1);
+            }
+
+            File destinationFilePath;
+            System.out.println("What is the destination file?");
+            path = userInput.nextLine();
+            destinationFilePath =  new File(path);
+            if (!destinationFilePath.exists()) {
+                System.out.println(path + " does not exist");
+                System.exit(1);
+            } else if (!destinationFilePath.isFile()) {
+                System.out.println(path + " is not a file");
+                System.exit(1);
+            }
+
+            try (Scanner sourceScanner = new Scanner(sourceFilePath.getAbsoluteFile());
+                 PrintWriter destinationWriter = new PrintWriter(destinationFilePath.getAbsoluteFile())) {
+                while (sourceScanner.hasNextLine()) {
+                    String line = sourceScanner.nextLine();
+                    destinationWriter.println(line.replaceAll(searchWord, replacementWord));
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
--- 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,35 @@
 package com.techelevator;
 
-public class FizzWriter {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-	public static void main(String[] args) {
+public class FizzWriter {
+			public static void main(String[] args) {
+
+			File destinationFile = new File("src/test/resources/fizzbuzz.txt");
+
+			try (PrintWriter destinationWriter = new PrintWriter(destinationFile.getAbsoluteFile())) {
+				for (int i = 1; i <= 300; i++) {
+					boolean isDivisibleBy5 = i % 5 == 0;
+					boolean isDivisibleBy3 = i % 3 == 0;
+					if (isDivisibleBy5 && isDivisibleBy3) {
+						destinationWriter.println("FizzBuzz");
+					} else if (isDivisibleBy3) {
+						destinationWriter.println("Fizz");
+					} else if (isDivisibleBy5) {
+						destinationWriter.println("Buzz");
+					} else {
+						destinationWriter.println(Integer.toString(i));
+					}
+				}
+
+				System.out.println("FizzBuzz.txt has been created.");
+
+			} catch (FileNotFoundException fileNotFoundException) {
+				System.out.print(fileNotFoundException.getStackTrace());
+			}
+		}
 
 	}
-
-}
